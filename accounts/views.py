a2ad27607658dbf19958afed496473166638dc3e
Jahangirruet <jahangir.2012@outlook.com>
2022-02-03 23:38:50 +0600

    account registration and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,51 @@
 from django.shortcuts import render
+from django.conf import ENVIRONMENT_VARIABLE
+from django.http import request
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+# Create your views here.
+from django.http import HttpResponse
 
 # Create your views here.
 
-def signup(request):
-    return render(request,'signup.html')
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name  = request.POST['last_name']
+        username  = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email  = request.POST['email']
 
-
+        if password1==password2:
+            user =  User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+            user.save();
+            print("user created")
+            return redirect('login')
+            #return render(request,'login.html')
+        else:
+            print('password not matching')        
+            #return render(request,'signup.html')
+    else :    
+        return render(request,'signup.html')    
+      
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST('username')
+        password = request.POST('password')
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None :
+            auth.login(request, user)
+            return redirect("/")      
+    else :
+        return render(request, "login.html")
+
+
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
